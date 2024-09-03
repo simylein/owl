3bbb2f7f47df1653207f90028eaf4bea52032d88
simylein <67859356+simylein@users.noreply.github.com>
2024-09-03 20:20:35 +0200

    chore(setup): gitignore and build config

---
+++ build.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+
+pub fn build(builder: *std.Build) void {
+    const executable = builder.addExecutable(.{
+        .name = "owl",
+        .target = builder.standardTargetOptions(.{}),
+        .optimize = builder.standardOptimizeOption(.{}),
+        .root_source_file = builder.path("lib/main.zig"),
+    });
+    builder.exe_dir = "";
+    builder.installArtifact(executable);
+}
