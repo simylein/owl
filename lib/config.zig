d182ef246222297b4c7b4d22dbbd715d7aac86b2
simylein <67859356+simylein@users.noreply.github.com>
2024-09-11 23:33:38 +0200

    refactor(main): use a config instead for simpler code

---
+++ lib/config.zig
@@ -0,0 +1,102 @@
+const std = @import("std");
+const logger = @import("logger.zig");
+
+pub const App = struct {
+    name: []u8,
+    address: std.net.Address,
+    timeout: u8,
+    interval: u8,
+};
+
+pub fn init(comptime path: []const u8) std.ArrayList(App) {
+    logger.trace("opening {s}...", .{path});
+    const file = std.fs.cwd().openFile(path, .{ .mode = std.fs.File.OpenMode.read_only, .lock = std.fs.File.Lock.exclusive }) catch |err| {
+        logger.panic("could not open {s} ({s})", .{ path, @errorName(err) });
+        std.process.exit(1);
+    };
+    defer file.close();
+    const stat = file.stat() catch |err| {
+        logger.panic("could not stat {s} ({s})", .{ path, @errorName(err) });
+        std.process.exit(1);
+    };
+    const buffer = std.heap.c_allocator.alloc(u8, stat.size) catch |err| {
+        logger.panic("could not allocate {d} bytes ({s})", .{ stat.size, @errorName(err) });
+        std.process.exit(1);
+    };
+    defer std.heap.c_allocator.free(buffer);
+    const read = file.read(buffer) catch |err| {
+        logger.panic("could not read {s} file ({s})", .{ path, @errorName(err) });
+        std.process.exit(1);
+    };
+    logger.debug("read {d} bytes from {s}", .{ read, path });
+    const content = buffer[0..read];
+    logger.debug("parsing configuration...", .{});
+    var apps = std.ArrayList(App).init(std.heap.c_allocator);
+    var index: u8 = 0;
+    var tokenizer = std.mem.tokenize(u8, content, "\n");
+    while (tokenizer.next()) |line| : (index += 1) {
+        logger.trace("parsing app {d}...", .{index});
+        var app = App{ .name = undefined, .address = undefined, .timeout = undefined, .interval = undefined };
+        var token = std.mem.tokenize(u8, line, " ");
+        const name = if (token.next()) |name| name else {
+            logger.fault("app {d} must have a name", .{index});
+            std.process.exit(1);
+        };
+        const host = if (token.next()) |host| host else {
+            logger.fault("app {d} must have a host", .{index});
+            std.process.exit(1);
+        };
+        const port = if (token.next()) |port| port else {
+            logger.fault("app {d} must have a port", .{index});
+            std.process.exit(1);
+        };
+        const timeout = if (token.next()) |timeout| timeout else {
+            logger.fault("app {d} must have an timeout", .{index});
+            std.process.exit(1);
+        };
+        const interval = if (token.next()) |interval| interval else {
+            logger.fault("app {d} must have an interval", .{index});
+            std.process.exit(1);
+        };
+        if (name.len < 2 or name.len > 16) {
+            logger.fault("app {d} name must be between 2 and 16 characters", .{index});
+            std.process.exit(1);
+        }
+        for (name) |char| {
+            if ((char < 97 or char > 122) and char != 45) {
+                logger.fault("app {d} name must contain lowercase letters or hyphons", .{index});
+                std.process.exit(1);
+            }
+        }
+        app.name = std.heap.c_allocator.alloc(u8, name.len) catch |err| {
+            logger.panic("could not allocate {d} bytes ({s})", .{ name.len, @errorName(err) });
+            std.process.exit(1);
+        };
+        std.mem.copyForwards(u8, app.name, name);
+        app.address = std.net.Address.resolveIp(host, std.fmt.parseInt(u16, port, 10) catch {
+            logger.fault("app {d} port must be between 0 and 65535", .{index});
+            std.process.exit(1);
+        }) catch |err| {
+            logger.fault("app {d} address must be valid ({s})", .{ index, @errorName(err) });
+            std.process.exit(1);
+        };
+        app.timeout = std.fmt.parseInt(u8, timeout, 10) catch {
+            logger.fault("app {d} timeout must be between 0 and 255", .{index});
+            std.process.exit(1);
+        };
+        app.interval = std.fmt.parseInt(u8, interval, 10) catch {
+            logger.fault("app {d} interval must be between 0 and 255", .{index});
+            std.process.exit(1);
+        };
+        if (app.timeout > app.interval) {
+            logger.fault("app {d} timeout must be smaller than interval", .{index});
+            std.process.exit(1);
+        }
+        apps.append(app) catch |err| {
+            logger.panic("could not allocate app {s} ({s})", .{ app.name, @errorName(err) });
+            std.process.exit(1);
+        };
+    }
+    logger.info("configuration holds {d} apps", .{apps.items.len});
+    return apps;
+}
--- lib/main.zig
+++ lib/main.zig
@@ -1,90 +1,47 @@
 const std = @import("std");
-const arguments = @import("arguments.zig");
-const database = @import("database.zig");
+const config = @import("config.zig");
 const logger = @import("logger.zig");
 
-const path = "owl.db";
+const host = "127.0.0.1";
+const port = 4000;
 
 pub fn main() void {
-    var args = arguments.init();
-    defer args.deinit();
+    const apps = config.init("owl.cfg");
 
-    if (args.items.len < 1) {
-        logger.warn("not enough arguments", .{});
-        logger.info("available options", .{});
-        logger.info("init", .{});
-        logger.info("list", .{});
-        logger.info("add name", .{});
-        logger.info("remove name", .{});
+    for (apps.items) |app| {
+        logger.debug("spawning {s} healthcheck thread...", .{app.name});
+        _ = std.Thread.spawn(.{ .allocator = std.heap.c_allocator }, healthcheck, .{&app}) catch |err| {
+            logger.panic("could not spawn thread for app {s} ({s})", .{ app.name, @errorName(err) });
+            std.process.exit(1);
+        };
+    }
+
+    logger.info("starting http server...", .{});
+
+    const address = std.net.Address.resolveIp(host, port) catch |err| {
+        logger.panic("could not resolve address {s}:{d} ({s})", .{ host, port, @errorName(err) });
         std.process.exit(1);
+    };
+    var server = address.listen(.{ .kernel_backlog = 256, .reuse_address = true }) catch |err| {
+        logger.panic("could not listen on {} ({s})", .{ address, @errorName(err) });
+        std.process.exit(1);
+    };
+
+    logger.info("listening on {}", .{server.listen_address});
+
+    while (true) {
+        const connection = server.accept() catch |err| {
+            logger.fault("could not accept client ({s})", .{@errorName(err)});
+            continue;
+        };
+        logger.debug("connection from {}", .{connection.address});
     }
+}
 
-    if (std.mem.eql(u8, args.items[0], "init")) {
-        logger.trace("initialising {s}...", .{path});
-        const file = std.fs.cwd().createFile(path, .{ .exclusive = true }) catch |err| {
-            logger.fault("could not initialise {s} ({s})", .{ path, @errorName(err) });
-            std.process.exit(1);
-        };
-        file.close();
-        logger.debug("initialised {s}", .{path});
-        std.process.exit(0);
-    }
-
-    var db = database.init(path);
-    defer db.deinit();
-
-    if (std.mem.eql(u8, args.items[0], "list")) {
-        logger.info("watching {d} applications", .{db.apps.items.len});
-        if (db.apps.items.len > 0) {
-            logger.info("id name", .{});
-        }
-        for (db.apps.items) |app| {
-            logger.info("{d} {s}", .{ app.id, app.name });
-        }
-        std.process.exit(0);
-    }
-
-    if (std.mem.eql(u8, args.items[0], "add")) {
-        if (args.items.len != 2) {
-            logger.warn("invalid amount of arguments for adding", .{});
-            logger.info("usage: add name", .{});
-            std.process.exit(1);
-        }
-        const name = std.heap.c_allocator.alloc(u8, args.items[1].len) catch |err| {
-            logger.panic("could not allocate {d} bytes ({s})", .{ args.items[1].len, @errorName(err) });
-            std.process.exit(1);
-        };
-        std.mem.copyForwards(u8, name, args.items[1]);
-        if (name.len < 2 or name.len > 16) {
-            logger.warn("name must be between 2 and 16 characters", .{});
-            std.process.exit(1);
-        }
-        if (db.apps.items.len >= 255) {
-            logger.warn("can not store more than 255 applications", .{});
-            std.process.exit(1);
-        }
-        const id: u8 = @intCast(db.apps.items.len);
-        db.insert(.{ .id = id, .name = name }) catch |err| {
-            logger.fault("could not insert application ({s})", .{@errorName(err)});
-            std.process.exit(1);
-        };
-        std.process.exit(0);
-    }
-
-    if (std.mem.eql(u8, args.items[0], "remove")) {
-        if (args.items.len != 2) {
-            logger.warn("invalid amount of arguments for removing", .{});
-            logger.info("usage: remove id", .{});
-            std.process.exit(1);
-        }
-        const id = std.fmt.parseInt(u8, args.items[1], 10) catch |err| {
-            logger.warn("id must be between 0 and 255 ({s})", .{@errorName(err)});
-            std.process.exit(1);
-        };
-        db.delete(id) catch |err| {
-            logger.fault("could not delete application ({s})", .{@errorName(err)});
-            std.process.exit(1);
-        };
-        std.process.exit(0);
+pub fn healthcheck(app: *const config.App) void {
+    while (true) {
+        logger.trace("app {s} healthcheck running...", .{app.name});
+        const time: u64 = @intCast(app.interval);
+        std.time.sleep(time * std.time.ns_per_s);
     }
 }
