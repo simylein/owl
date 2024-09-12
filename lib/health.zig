809ac291bf60599d16c6edf30fc0d2357e366912
simylein <67859356+simylein@users.noreply.github.com>
2024-09-12 10:15:27 +0200

    feat(main): actually do the health checking

---
+++ lib/health.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+const arguments = @import("arguments.zig");
+const config = @import("config.zig");
+const database = @import("database.zig");
+const logger = @import("logger.zig");
+
+fn connect(address: std.net.Address) ?std.net.Stream {
+    const stream = std.net.tcpConnectToAddress(address) catch |err| {
+        logger.warn("could not connect to {} ({s})", .{ address, @errorName(err) });
+        return null;
+    };
+    return stream;
+}
+
+pub fn check(app: config.App, data: *database.Data) void {
+    std.time.sleep(std.time.ns_per_s);
+
+    while (true) {
+        logger.trace("healthchecking {s}...", .{app.name});
+        const start = std.time.nanoTimestamp();
+        const stream = connect(app.address);
+        if (stream) |socket| {
+            defer socket.close();
+        }
+        const stop = std.time.nanoTimestamp();
+
+        const timestamp: u64 = @intCast(@divFloor(start, 1000_000_000));
+        const latency: u32 = @intCast(stop - start);
+        const healthy = if (stream != null) true else false;
+
+        if (healthy) {
+            logger.info("app {s} is healthy ({d}ns)", .{ app.name, latency });
+        } else {
+            logger.warn("app {s} is unhealthy ({d}ns)", .{ app.name, latency });
+        }
+
+        data.insert(.{ .app_id = app.id, .timestamp = timestamp, .latency = latency, .healthy = healthy }) catch |err| {
+            logger.fault("could not insert data for app {s} ({s})", .{ app.name, @errorName(err) });
+        };
+
+        const interval: u64 = @intCast(app.interval);
+        const wait = interval * std.time.ns_per_s - latency;
+
+        if (wait > 0) {
+            std.time.sleep(wait);
+        }
+    }
+}
--- lib/main.zig
+++ lib/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const arguments = @import("arguments.zig");
 const config = @import("config.zig");
 const database = @import("database.zig");
+const health = @import("health.zig");
 const logger = @import("logger.zig");
 
 pub fn main() void {
@@ -11,7 +12,7 @@ pub fn main() void {
 
     for (apps.items) |app| {
         logger.debug("spawning {s} healthcheck thread...", .{app.name});
-        const thread = std.Thread.spawn(.{ .allocator = std.heap.c_allocator }, healthcheck, .{ app, &data }) catch |err| {
+        const thread = std.Thread.spawn(.{ .allocator = std.heap.c_allocator }, health.check, .{ app, &data }) catch |err| {
             logger.panic("could not spawn thread for app {s} ({s})", .{ app.name, @errorName(err) });
             std.process.exit(1);
         };
@@ -40,32 +41,6 @@ pub fn main() void {
         defer {
             logger.trace("closed connection to {}", .{connection.address});
             connection.stream.close();
-        }
-    }
-}
-
-pub fn healthcheck(app: config.App, data: *database.Data) void {
-    std.time.sleep(std.time.ns_per_s);
-
-    while (true) {
-        logger.trace("healthchecking {s}...", .{app.name});
-        const start = std.time.nanoTimestamp();
-
-        const stop = std.time.nanoTimestamp();
-
-        const timestamp: u64 = @intCast(@divFloor(start, 1000_000_000));
-        const latency: u32 = @intCast(stop - start);
-        const healthy: bool = true;
-
-        data.insert(.{ .app_id = app.id, .timestamp = timestamp, .latency = latency, .healthy = healthy }) catch |err| {
-            logger.fault("could not insert data for app {s} ({s})", .{ app.name, @errorName(err) });
-        };
-
-        const interval: u64 = @intCast(app.interval);
-        const wait = interval * std.time.ns_per_s - latency;
-
-        if (wait > 0) {
-            std.time.sleep(wait);
         }
     }
 }
