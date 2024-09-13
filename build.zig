d88a39fd73a6cf8521af5b7243f369ec5908daf8
simylein <67859356+simylein@users.noreply.github.com>
2024-09-13 09:28:29 +0200

    fix(build): link c standard library

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub fn build(builder: *std.Build) void {
     const executable = builder.addExecutable(.{
         .name = "owl",
+        .link_libc = true,
         .target = builder.standardTargetOptions(.{}),
         .optimize = builder.standardOptimizeOption(.{}),
         .root_source_file = builder.path("lib/main.zig"),
